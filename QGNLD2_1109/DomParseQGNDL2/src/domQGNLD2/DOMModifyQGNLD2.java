bcbc8deb1cb7d50a749a7484e58290f1ea38b497
TZeteny <99351536+TZeteny@users.noreply.github.com>
2022-11-09 11:28:33 +0100

    Add files via upload

---
+++ QGNLD2_1109/DomParseQGNDL2/src/domQGNLD2/DOMModifyQGNLD2.java
@@ -0,0 +1,38 @@
+package domQGNLD2;
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.ParserConfigurationException;
+
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
+
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.Element;
+
+import org.xml.sax.SAXException;
+
+
+public class DOMModifyQGNLD2 {
+
+	public static void main(String[] args) {
+		
+		try {
+			File inputFile = new File("carsQGNLD2.xml");
+			
+			DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
+			
+			
+		}
+		
+	}
+
+}
