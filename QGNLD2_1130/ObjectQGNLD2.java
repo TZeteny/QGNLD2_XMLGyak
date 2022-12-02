2420546144795f478511e8b89e6ff9eddbea1229
TZeteny <99351536+TZeteny@users.noreply.github.com>
2022-12-02 19:29:12 +0100

    Create ObjectQGNLD2.java

---
+++ QGNLD2_1130/ObjectQGNLD2.java
@@ -0,0 +1,20 @@
+package QGNLD2;
+
+import java.io.*;
+
+import org.json.simple.*;
+
+public class ObjectQGNLD2 {
+  
+	public static void main(String[] args) {
+    
+		JSONObject j = new JSONObject(); 
+    
+		j.put("neptunkod", "kkklll");
+		j.put("hallgato", "hallgato neve");
+		j.put("szak", "PTI");
+		System.out.println(j.toString());
+	
+	}	
+ 
+}
